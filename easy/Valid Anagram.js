2d62f1b173a4c7cd511fe10002b4f0699cc393a5
Valefar Frontend Developer <inzhener.intellect@gmail.com>
2019-02-17 13:37:10 +0700

    Create Valid Anagram.js

---
+++ easy/Valid Anagram.js
@@ -0,0 +1,20 @@
+/*
+Given two strings s and t , write a function to determine if t is an anagram of s.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+Note:
+You may assume the string contains only lowercase alphabets.
+
+Follow up:
+What if the inputs contain unicode characters? How would you adapt your solution to such case?
+*/
+var isAnagram = function(original, test) {
+    return test.toLowerCase().split("").sort().join("") === original.toLowerCase().split("").sort().join("");
+};
