7c542261d76641d06d0fd36fef2ac7174d552c55
Valefar React Developer <inzhener.intellect@gmail.com>
2019-09-18 15:04:19 +0700

    Create 627. Swap Salary.js

---
+++ easy/627. Swap Salary.js
@@ -0,0 +1,22 @@
+/*
+Note that you must write a single update statement, DO NOT write any select statement for this problem.
+
+ 
+
+Example:
+
+| id | name | sex | salary |
+|----|------|-----|--------|
+| 1  | A    | m   | 2500   |
+| 2  | B    | f   | 1500   |
+| 3  | C    | m   | 5500   |
+| 4  | D    | f   | 500    |
+After running your update statement, the above salary table should have the following rows:
+| id | name | sex | salary |
+|----|------|-----|--------|
+| 1  | A    | f   | 2500   |
+| 2  | B    | m   | 1500   |
+| 3  | C    | f   | 5500   |
+| 4  | D    | m   | 500    |
+*/
+update salary set sex = IF (sex = "m", "f", "m");
