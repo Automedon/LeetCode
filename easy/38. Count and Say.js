ca3e33fb06ec5f651e62a5162e32619c3769bf77
Valefar React Developer <inzhener.intellect@gmail.com>
2019-10-15 11:35:32 +0700

    Create 38. Count and Say.js

---
+++ easy/38. Count and Say.js
@@ -0,0 +1,28 @@
+/*
+The count-and-say sequence is the sequence of integers with the first five terms as following:
+
+1.     1
+2.     11
+3.     21
+4.     1211
+5.     111221
+1 is read off as "one 1" or 11.
+11 is read off as "two 1s" or 21.
+21 is read off as "one 2, then one 1" or 1211.
+
+Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the count-and-say sequence.
+
+Note: Each term of the sequence of integers will be represented as a string.
+
+ 
+
+Example 1:
+
+Input: 1
+Output: "1"
+Example 2:
+
+Input: 4
+Output: "1211"
+*/
+let countAndSay = (n,str='1')=> Array.from({length:n-1},(v,i)=>str=str.replace(/(.)\1*/g,m=> m.length+m[0])).reverse()[0]||str
