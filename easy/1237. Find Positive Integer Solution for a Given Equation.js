cf0c5855bb97cbf389995bac726ee15726486ada
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2020-05-14 18:11:45 +0700

    Create 1237. Find Positive Integer Solution for a Given Equation.js

---
+++ easy/1237. Find Positive Integer Solution for a Given Equation.js
@@ -0,0 +1,40 @@
+/*
+Given a function  f(x, y) and a value z, return all positive integer pairs x and y where f(x,y) == z.
+
+The function is constantly increasing, i.e.:
+
+f(x, y) < f(x + 1, y)
+f(x, y) < f(x, y + 1)
+The function interface is defined like this: 
+
+interface CustomFunction {
+public:
+  // Returns positive integer f(x, y) for any given positive integer x and y.
+  int f(int x, int y);
+};
+For custom testing purposes you're given an integer function_id and a target z as input, where function_id represent one function from an secret internal list, on the examples you'll know only two functions from the list.  
+
+You may return the solutions in any order.
+
+ 
+
+Example 1:
+
+Input: function_id = 1, z = 5
+Output: [[1,4],[2,3],[3,2],[4,1]]
+Explanation: function_id = 1 means that f(x, y) = x + y
+Example 2:
+
+Input: function_id = 2, z = 5
+Output: [[1,5],[5,1]]
+Explanation: function_id = 2 means that f(x, y) = x * y
+*/
+var findSolution = function(customfunction, z) {
+    const arr = []
+    for (let i=1;i<=z;i++){
+     for (let j=1;j<=z;j++){
+     if (customfunction.f(i,j)===z) arr.push([i,j])
+     }   
+    }
+    return arr
+};
