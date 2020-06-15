59b7e6d7c3c68c940a1155da27c42915221cc035
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2020-06-15 21:27:59 +0700

    Create 1480. Running Sum of 1d Array.js

---
+++ easy/1480. Running Sum of 1d Array.js
@@ -0,0 +1,31 @@
+/*
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+Example 2:
+
+Input: nums = [1,1,1,1,1]
+Output: [1,2,3,4,5]
+Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+Example 3:
+
+Input: nums = [3,1,2,10,1]
+Output: [3,4,6,16,17]
+ 
+
+Constraints:
+
+1 <= nums.length <= 1000
+-10^6 <= nums[i] <= 10^6
+*/
+var runningSum = function(nums) {
+    return nums.map((v,i)=>nums.slice(0,i+1).reduce((a,b)=>a+b,0))
+};
