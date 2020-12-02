78f677b086edeea273d5814784aa0ddd2cedf615
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2020-12-02 21:39:45 +0700

    Create 171. Excel Sheet Column Number.js

---
+++ easy/171. Excel Sheet Column Number.js
@@ -0,0 +1,69 @@
+/*
+Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+For example:
+
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28 
+    ...
+Example 1:
+
+Input: "A"
+Output: 1
+Example 2:
+
+Input: "AB"
+Output: 28
+Example 3:
+
+Input: "ZY"
+Output: 701
+ 
+
+Constraints:
+
+1 <= s.length <= 7
+s consists only of uppercase English letters.
+s is between "A" and "FXSHRXW".
+*/
+const letter = {
+    A: 1,
+    B: 2,
+    C: 3,
+    D: 4,
+    E: 5,
+    F: 6,
+    G: 7,
+    H: 8,
+    I: 9,
+    J: 10,
+    K: 11,
+    L: 12,
+    M: 13,
+    N: 14,
+    O: 15,
+    P: 16,
+    Q: 17,
+    R: 18,
+    S: 19,
+    T: 20,
+    U: 21,
+    V: 22,
+    W: 23,
+    X: 24,
+    Y: 25,
+    Z: 26
+}
+
+var titleToNumber = function(s) {
+    return s.split('').reduceRight((finalColumn, currentNum, i) => {
+        if(i === s.length - 1) return letter[currentNum];
+        
+        return finalColumn + letter[currentNum] * Math.pow(26,s.length - 1 - i);
+    }, 0);
+};
