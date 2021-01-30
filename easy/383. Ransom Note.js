86d6a53ad37311ca5ac8e03b6d6b193a281c4740
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2021-01-30 23:05:24 +0700

    Create 383. Ransom Note.js

---
+++ easy/383. Ransom Note.js
@@ -0,0 +1,36 @@
+/*
+Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+
+Each letter in the magazine string can only be used once in your ransom note.
+
+ 
+
+Example 1:
+
+Input: ransomNote = "a", magazine = "b"
+Output: false
+Example 2:
+
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+Example 3:
+
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+ 
+
+Constraints:
+
+You may assume that both strings contain only lowercase letters.
+*/
+var canConstruct = function(ransomNote, magazine) {
+    const arr = magazine.split('');
+    for (let i = 0; i < ransomNote.length; i++) {
+        if (arr.indexOf(ransomNote[i]) === -1) {
+            return false;
+        } else {
+            arr.splice(arr.indexOf(ransomNote[i]), 1);
+        }
+    }
+    return true;
+};
