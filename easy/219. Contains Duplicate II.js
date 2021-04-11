a0653e1eac9093abafb71c560ccc6091af22b9d5
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2021-04-11 18:43:42 +0700

    Create 219. Contains Duplicate II.js

---
+++ easy/219. Contains Duplicate II.js
@@ -0,0 +1,36 @@
+/*
+Share
+Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1], k = 3
+Output: true
+Example 2:
+
+Input: nums = [1,0,1,1], k = 1
+Output: true
+Example 3:
+
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+0 <= k <= 105
+*/
+var containsNearbyDuplicate = function(nums, k) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(nums[i]) && i - map.get(nums[i]) <= k) {
+            return true;
+        }
+        map.set(nums[i], i);
+    }
+    return false;
+};
