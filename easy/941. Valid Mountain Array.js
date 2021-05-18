3076c1ffc18de2b746ef42e6784f0e31b0b031a9
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2021-05-18 20:58:31 +0700

    Create 941. Valid Mountain Array.js

---
+++ easy/941. Valid Mountain Array.js
@@ -0,0 +1,45 @@
+/*
+Given an array of integers arr, return true if and only if it is a valid mountain array.
+
+Recall that arr is a mountain array if and only if:
+
+arr.length >= 3
+There exists some i with 0 < i < arr.length - 1 such that:
+arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+
+ 
+
+Example 1:
+
+Input: arr = [2,1]
+Output: false
+Example 2:
+
+Input: arr = [3,5,5]
+Output: false
+Example 3:
+
+Input: arr = [0,3,2,1]
+Output: true
+ 
+
+Constraints:
+
+1 <= arr.length <= 104
+0 <= arr[i] <= 104
+*/
+var validMountainArray = function(arr) {
+    if(arr.length<3) return false
+    const max = Math.max(...arr)
+    const indexOfMax = arr.indexOf(max)
+    const left = arr.slice(0,indexOfMax)
+    const right = arr.slice(indexOfMax+1)
+    if(!left.length || !right.length) return false
+    if(left.join('')!==left.slice().sort((a,b)=>a-b).join('')) return false
+    if(right.join('')!==right.slice().sort((a,b)=>b-a).join('')) return false
+    for(let i = 0;i<arr.length-1;i++){
+        if(arr[i]===arr[i+1]) return false
+    }
+    return true
+};
