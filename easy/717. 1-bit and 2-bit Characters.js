2ff860af6ef676eb80cf36fce3919383c8da79e3
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-02-14 11:01:31 +0700

    Create 717. 1-bit and 2-bit Characters.js

---
+++ easy/717. 1-bit and 2-bit Characters.js
@@ -0,0 +1,37 @@
+/*
+We have two special characters:
+
+The first character can be represented by one bit 0.
+The second character can be represented by two bits (10 or 11).
+Given a binary array bits that ends with 0, return true if the last character must be a one-bit character.
+
+ 
+
+Example 1:
+
+Input: bits = [1,0,0]
+Output: true
+Explanation: The only way to decode it is two-bit character and one-bit character.
+So the last character is one-bit character.
+Example 2:
+
+Input: bits = [1,1,1,0]
+Output: false
+Explanation: The only way to decode it is two-bit character and two-bit character.
+So the last character is not one-bit character.
+ 
+
+Constraints:
+
+1 <= bits.length <= 1000
+bits[i] is either 0 or 1.
+*/
+
+var isOneBitCharacter = function(bits) {
+  let i = 0;
+  while (i < bits.length - 1) {
+    if (bits[i] === 1) i++;
+    i++;
+  }
+  return bits[i] === 0;
+};
