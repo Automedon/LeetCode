8110edd4dcfb5abae7ee2997430706502a4ed599
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-02-15 11:35:53 +0700

    Create 1005. Maximize Sum Of Array After K Negations.js

---
+++ easy/1005. Maximize Sum Of Array After K Negations.js
@@ -0,0 +1,46 @@
+/*
+Given an integer array nums and an integer k, modify the array in the following way:
+
+choose an index i and replace nums[i] with -nums[i].
+You should apply this process exactly k times. You may choose the same index i multiple times.
+
+Return the largest possible sum of the array after modifying it in this way.
+
+ 
+
+Example 1:
+
+Input: nums = [4,2,3], k = 1
+Output: 5
+Explanation: Choose index 1 and nums becomes [4,-2,3].
+Example 2:
+
+Input: nums = [3,-1,0,2], k = 3
+Output: 6
+Explanation: Choose indices (1, 2, 2) and nums becomes [3,1,0,2].
+Example 3:
+
+Input: nums = [2,-3,-1,5,-4], k = 2
+Output: 13
+Explanation: Choose indices (1, 4) and nums becomes [2,3,-1,5,4].
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-100 <= nums[i] <= 100
+1 <= k <= 104
+*/
+
+var largestSumAfterKNegations = function(A, K) {
+    A = A.sort((a, b) => a - b);
+    let i = 0;
+    
+    while (K > 0) {
+        if (A[i] === 0) break;
+        A[i] = -A[i];
+        if (A[i] > A[i + 1]) i++;
+        K--;
+    }
+    return A.reduce((a, b) => a + b);
+};
