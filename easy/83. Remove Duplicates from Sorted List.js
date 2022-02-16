fe01fdfd62f64e0c12c9c099a77906df133beaa1
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-02-16 20:01:27 +0700

    Create 83. Remove Duplicates from Sorted List.js

---
+++ easy/83. Remove Duplicates from Sorted List.js
@@ -0,0 +1,36 @@
+/*
+Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,1,2]
+Output: [1,2]
+Example 2:
+
+
+Input: head = [1,1,2,3,3]
+Output: [1,2,3]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 300].
+-100 <= Node.val <= 100
+The list is guaranteed to be sorted in ascending order.
+*/
+
+var deleteDuplicates = function(head) {
+    let current = head;
+    while (current != null && current.next != null) {
+        if (current.next.val == current.val) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+    
+    return head
+};
