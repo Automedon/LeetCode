d186f39f6893e1a0750040bf152a82845cee5da2
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-03-08 21:49:52 +0700

    Update 100. Same Tree.js

--- easy/100. Same Tree.js
+++ easy/100. Same Tree.js
@@ -4,7 +4,6 @@ Given two binary trees, write a function to check if they are the same or not.
 Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
 
 Example 1:
-
 Input:     1         1
           / \       / \
          2   3     2   3
