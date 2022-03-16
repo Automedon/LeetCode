888708a708a86ebb7caa34112f4f4f641956a4a7
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-03-16 09:05:56 +0700

    Update 1018. Binary Prefix Divisible By 5.js

--- easy/1018. Binary Prefix Divisible By 5.js
+++ easy/1018. Binary Prefix Divisible By 5.js
@@ -1,6 +1,5 @@
 /*
 Given an array A of 0s and 1s, consider N_i: the i-th subarray from A[0] to A[i] interpreted as a binary number (from most-significant-bit to least-significant-bit.)
-
 Return a list of booleans answer, where answer[i] is true if and only if N_i is divisible by 5.
 
 Example 1:
