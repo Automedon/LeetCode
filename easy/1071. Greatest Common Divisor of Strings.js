25322872b16cab12a28746c8af50f475ad56bc6e
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-04-10 18:00:33 +0300

    Update 1071. Greatest Common Divisor of Strings.js

--- easy/1071. Greatest Common Divisor of Strings.js
+++ easy/1071. Greatest Common Divisor of Strings.js
@@ -4,7 +4,6 @@ For two strings s and t, we say "t divides s" if and only if s = t + ... + t  (t
 Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
 
  
-
 Example 1:
 
 Input: str1 = "ABCABC", str2 = "ABC"
