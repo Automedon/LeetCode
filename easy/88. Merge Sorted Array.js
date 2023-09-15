77de8a60af8ed18418fc3ff22f811c230ca7a61e
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2023-09-15 13:54:27 +0300

    Update 88. Merge Sorted Array.js

--- easy/88. Merge Sorted Array.js
+++ easy/88. Merge Sorted Array.js
@@ -35,3 +35,10 @@ var merge = function (nums1, m, nums2, n) {
   }
   return nums1;
 };
+
+const merge = function(nums1, m, nums2, n) {
+    for(let i=m,j=0;j<n;j++,i++){
+        nums1[i] = nums2[j]
+    }
+    return nums1.sort((a,b)=>a-b);
+};
