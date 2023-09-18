bf0884642b991cc51ad782baad5612198080e0de
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2023-09-18 09:04:33 +0300

    Create 359. Logger Rate Limiter.js

---
+++ easy/359. Logger Rate Limiter.js
@@ -0,0 +1,29 @@
+/*
+Design a logger system that receives a stream of messages along with their timestamps. Each unique message should only be printed at most every 10 seconds (i.e. a message printed at timestamp t will prevent other identical messages from being printed until timestamp t + 10).
+
+All messages will come in chronological order. Several messages may arrive at the same timestamp.
+
+Implement the Logger class:
+
+Logger() Initializes the logger object.
+bool shouldPrintMessage(int timestamp, string message) Returns true if the message should be printed in the given timestamp, otherwise returns false.
+*/
+
+
+var Logger = function() {
+    this.messages = new Map()
+};
+
+/** 
+ * @param {number} timestamp 
+ * @param {string} message
+ * @return {boolean}
+ */
+Logger.prototype.shouldPrintMessage = function(timestamp, message) {
+    const messages = this.messages
+    if(!messages.get(message) || messages.get(message) <= timestamp){
+        messages.set(message,timestamp + 10)
+        return true
+    }
+    return false
+};
