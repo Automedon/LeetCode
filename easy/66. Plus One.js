71fb6e1e014dd0e1f9b55a719e71290fdf626992
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2023-12-24 12:48:54 +0300

    Update 66. Plus One.js

--- easy/66. Plus One.js
+++ easy/66. Plus One.js
@@ -17,11 +17,5 @@ Output: [4,3,2,2]
 Explanation: The array represents the integer 4321.
 */
 var plusOne = function(digits) {
-    var carry = 1;
-    for(var i=digits.length-1; i>=0; i--){
-        digits[i] = digits[i] + carry;
-        digits[i] = (digits[i]>=10)?(carry=1, digits[i]-10):(carry=0, digits[i]);
-    }
-    if(carry){ digits.unshift(1); } // note
-    return digits;
+    return (BigInt(digits.join(''))+1n).toString().split('').map(v=>parseInt(v))
 };
