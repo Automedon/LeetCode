9a33b0b37d187b1ce2899d1ffa5381c2bb4b7294
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2023-12-25 23:25:04 +0300

    Update 118. Pascal's Triangle.js

--- easy/118. Pascal's Triangle.js
+++ easy/118. Pascal's Triangle.js
@@ -16,24 +16,32 @@ Output:
  [1,4,6,4,1]
 ]
 */
+/*
+Given an integer numRows, return the first numRows of Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+Example 1:
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+Example 2:
+
+Input: numRows = 1
+Output: [[1]]
+ 
+
+Constraints:
+
+1 <= numRows <= 30
+*/
 var generate = function(numRows) {
-    let resp = [];
-    let i = 0;
-    while (i < numRows) {
-        if (i === 0) {
-            resp.push([1]);
-        } else {
-            let newRow = [];
-            for (let j = 0; j <= resp[i-1].length; j++) {
-                if (j-1 < 0 || j === resp[i-1].length) {
-                    newRow.push(1);
-                } else {
-                    newRow.push(resp[i-1][j-1] + resp[i-1][j]);
-                }
-            }
-            resp.push(newRow);
+    const arr = [[1],[1,1]];
+    for(let i=2;i<numRows;i++){
+        for(let j=1;j<i;j++){
+            if(!arr[i]) arr[i]=[1];
+            arr[i].push(arr[i-1][j-1]+arr[i-1][j])
         }
-        i++;
+        arr[i].push(1)
     }
-    return resp;
+    
+    return arr.slice(0,numRows)
 };
