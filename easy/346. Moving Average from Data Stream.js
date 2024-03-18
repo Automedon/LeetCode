30d2bbe2580f7aed2de769351c2e7fe0e8f359f9
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2024-03-19 00:13:43 +0300

    Create 346. Moving Average from Data Stream.js

---
+++ easy/346. Moving Average from Data Stream.js
@@ -0,0 +1,47 @@
+/*
+Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
+
+Implement the MovingAverage class:
+
+MovingAverage(int size) Initializes the object with the size of the window size.
+double next(int val) Returns the moving average of the last size values of the stream.
+ 
+
+Example 1:
+
+Input
+["MovingAverage", "next", "next", "next", "next"]
+[[3], [1], [10], [3], [5]]
+Output
+[null, 1.0, 5.5, 4.66667, 6.0]
+
+Explanation
+MovingAverage movingAverage = new MovingAverage(3);
+movingAverage.next(1); // return 1.0 = 1 / 1
+movingAverage.next(10); // return 5.5 = (1 + 10) / 2
+movingAverage.next(3); // return 4.66667 = (1 + 10 + 3) / 3
+movingAverage.next(5); // return 6.0 = (10 + 3 + 5) / 3
+ 
+
+Constraints:
+
+1 <= size <= 1000
+-105 <= val <= 105
+At most 104 calls will be made to next.
+*/
+
+/**
+ * @param {number} size
+ */
+var MovingAverage = function(size) {
+    this.size = size;
+    this.arr = [];
+};
+
+MovingAverage.prototype.next = function(val) {
+    this.arr.push(val);
+    if(this.arr.length>this.size){
+      this.arr.shift()
+    }
+    return this.arr.reduce((a,b)=>a+b,0)/this.arr.length;
+};
