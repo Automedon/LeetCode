e4bc56baa71f7e35a28b52f9d27bef0ae553232e
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2024-04-05 22:31:52 +0300

    Create 1790. Check if One String Swap Can Make Strings Equal.js

---
+++ easy/1790. Check if One String Swap Can Make Strings Equal.js
@@ -0,0 +1,35 @@
+/*
+You are given two strings s1 and s2 of equal length. A string swap is an operation where you choose two indices in a string (not necessarily different) and swap the characters at these indices.
+
+Return true if it is possible to make both strings equal by performing at most one string swap on exactly one of the strings. Otherwise, return false.
+
+Example 1:
+
+Input: s1 = "bank", s2 = "kanb"
+Output: true
+Explanation: For example, swap the first character with the last character of s2 to make "bank".
+Example 2:
+
+Input: s1 = "attack", s2 = "defend"
+Output: false
+Explanation: It is impossible to make them equal with one string swap.
+Example 3:
+
+Input: s1 = "kelb", s2 = "kelb"
+Output: true
+Explanation: The two strings are already equal, so no string swap operation is required.
+ 
+Constraints:
+
+1 <= s1.length, s2.length <= 100
+s1.length == s2.length
+s1 and s2 consist of only lowercase English letters.
+*/
+var areAlmostEqual = function (s1, s2) {
+  let count = 0;
+  for (let i = 0; i < s1.length; i++){
+    if(s1[i]!==s2[i]) count++
+    if(count>2) return false;
+  }
+  return s1.split``.sort().join``===s2.split``.sort().join``
+};
