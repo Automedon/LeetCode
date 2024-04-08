ccb2439a591f9bad0a747aa938e1701b2c805295
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2024-04-08 22:30:05 +0300

    Update 100. Same Tree.js

--- easy/100. Same Tree.js
+++ easy/100. Same Tree.js
@@ -33,3 +33,14 @@ Output: false
 var isSameTree = function(p, q) {
     return JSON.stringify(p)===JSON.stringify(q)
 };
+
+var isSameTree = function (p, q) {
+  const dfs = (r1, r2) => {
+    if (!r1 && !r2) return true;
+    if (!r1 || !r2) return false;
+    if (r1.val !== r2.val) return false;
+    return dfs(r1.left, r2.left) && dfs(r1.right, r2.right)
+  }
+
+  return dfs(p, q)
+};
