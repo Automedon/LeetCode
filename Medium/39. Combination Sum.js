73aec88869f9f44c8bcc0398cc0201acef09bac0
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2024-04-12 23:25:26 +0300

    Create 39. Combination Sum.js

---
+++ Medium/39. Combination Sum.js
@@ -0,0 +1,48 @@
+/*
+Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the 
+frequency
+ of at least one of the chosen numbers is different.
+
+The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+Example 1:
+
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+Example 2:
+
+Input: candidates = [2,3,5], target = 8
+Output: [[2,2,2,2],[2,3,3],[3,5]]
+Example 3:
+
+Input: candidates = [2], target = 1
+Output: []
+ 
+
+Constraints:
+1 <= candidates.length <= 30
+2 <= candidates[i] <= 40
+All elements of candidates are distinct.
+1 <= target <= 40
+*/
+
+var combinationSum = function (candidates, target) {
+  const res = [];
+  const dfs = (i = 0, cur = [], total = 0) => {
+    if (target === total) return res.push(cur.slice());
+    if (target < total || i === candidates.length) return;
+    cur.push(candidates[i]);
+    dfs(i,cur,total + candidates[i]);
+    cur.pop();
+    dfs(i+1,cur,total)
+  }
+
+  dfs()
+  return res;
+};
