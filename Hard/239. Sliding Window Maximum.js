335ca6f181462bc6e493471cbed79acbfe64fcac
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2024-07-05 15:48:27 +0300

    Create 239. Sliding Window Maximum.js

---
+++ Hard/239. Sliding Window Maximum.js
@@ -0,0 +1,48 @@
+/*
+You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+Return the max sliding window.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+Output: [3,3,5,5,6,7]
+Explanation: 
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+1 <= k <= nums.length
+*/
+var maxSlidingWindow = function (nums, k) {
+  const maxes = [];
+  const q = [];
+  for (let i = 0; i < nums.length; i++) {
+    while (nums[i] > q.at(-1)) {
+      q.pop()
+    }
+    q.push(nums[i]);
+    const j = i + 1 - k
+    if (j >= 0) {
+      maxes.push(q[0])
+      if (nums[j] === q[0]) q.shift()
+    }
+  }
+  return maxes
+};
