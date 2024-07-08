29b4315c28791837f4e675459c9fe506778e0e7e
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2024-07-08 13:39:00 +0300

    Update 2. Add Two Numbers.js

--- Medium/2. Add Two Numbers.js
+++ Medium/2. Add Two Numbers.js
@@ -9,20 +9,19 @@ Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 Explanation: 342 + 465 = 807.
 */
-var addTwoNumbers = function(l1, l2) {
-    let l3 = new ListNode(0)
-    nodeSum(l1, l2, l3, 0)
-    function nodeSum(n1, n2, n3, extraSum) {
-        let currnnode = ((n1.val || 0) + (n2.val || 0) + extraSum) + ''
-        let currentExtraSum = 0
-        if (currnnode.length > 1) {
-            currentExtraSum = currnnode[0] * 1
-        }
-        n3.val = currnnode[currnnode.length - 1] * 1
-        if (n1.next || n2.next || currentExtraSum) {
-            n3.next = new ListNode(0)
-            nodeSum(n1.next || {}, n2.next || {}, n3.next, currentExtraSum)
-        }
-    }
-    return l3
+var addTwoNumbers = function (l1, l2) {
+  const dummy = new ListNode(0);
+  let cur = dummy;
+  let curry = 0;
+  while (l1 || l2 || curry) {
+    const val1 = l1?.val ?? 0;
+    const val2 = l2?.val ?? 0;
+    const sum = curry + val1 + val2;
+    curry = sum > 9 ? 1 : 0;
+    cur.next = new ListNode(sum % 10);
+    cur = cur.next;
+    if (l1) l1 = l1.next;
+    if (l2) l2 = l2.next;
+  }
+  return dummy.next;
 };
