5c41d22b082903c823f1be21a333a4f1ff3cf2d8
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2024-07-12 22:59:30 +0300

    Create 295. Find Median from Data Stream.js

---
+++ Hard/295. Find Median from Data Stream.js
@@ -0,0 +1,60 @@
+/*
+The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value, and the median is the mean of the two middle values.
+
+For example, for arr = [2,3,4], the median is 3.
+For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+Implement the MedianFinder class:
+
+MedianFinder() initializes the MedianFinder object.
+void addNum(int num) adds the integer num from the data stream to the data structure.
+double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+ 
+
+Example 1:
+
+Input
+["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+[[], [1], [2], [], [3], []]
+Output
+[null, null, null, 1.5, null, 2.0]
+
+Explanation
+MedianFinder medianFinder = new MedianFinder();
+medianFinder.addNum(1);    // arr = [1]
+medianFinder.addNum(2);    // arr = [1, 2]
+medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+medianFinder.addNum(3);    // arr[1, 2, 3]
+medianFinder.findMedian(); // return 2.0
+ 
+
+Constraints:
+
+-105 <= num <= 105
+There will be at least one element in the data structure before calling findMedian.
+At most 5 * 104 calls will be made to addNum and findMedian.
+ 
+
+Follow up:
+
+If all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+If 99% of all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+*/
+var MedianFinder = function () {
+  this.arr = []
+};
+
+MedianFinder.prototype.addNum = function (num) {
+  const arr = this.arr;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > num) {
+      return arr.splice(i, 0, num)
+    }
+  }
+  arr.push(num)
+};
+
+MedianFinder.prototype.findMedian = function () {
+  const l = this.arr.length;
+  if (l % 2 !== 0) return this.arr[Math.floor(l / 2)]
+  return (this.arr[l / 2] + this.arr[(l / 2) - 1]) / 2
+};
