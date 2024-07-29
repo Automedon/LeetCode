46b5024c00d7801dea7bd0ad1fad7412bfc3536f
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2024-07-29 21:47:27 +0300

    Create 56. Merge Intervals.js

---
+++ Medium/56. Merge Intervals.js
@@ -0,0 +1,37 @@
+/*
+Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+ 
+
+Example 1:
+
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+Example 2:
+
+Input: intervals = [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+ 
+
+Constraints:
+
+1 <= intervals.length <= 104
+intervals[i].length == 2
+0 <= starti <= endi <= 104
+*/
+
+var merge = function (intervals) {
+    intervals.sort((a, b) => a[0] - b[0]);
+    for (let i = 1; i < intervals.length; i++) {
+        let [first, second] = intervals[i];
+        let [prevF, prevS] = intervals[i - 1];
+        if (first <= prevS) {
+            intervals[i - 1][1] = Math.max(second, prevS)
+            intervals.splice(i, 1);
+            i--
+        }
+    }
+    return intervals
+};
